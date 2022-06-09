357c771debee5fd8dd1f2d4efaa4ea91b36b5015
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-06-09 18:26:43 +0600

    finish about

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -2,13 +2,14 @@ import React from 'react';
 import opi2 from '../../../asset/picture/opi2.jpg';
 const About = () => {
     return (
-        <div className='mt-20 grid lg:grid-cols-2 sm:grid-cols-1 lg:px-24 px-4'>
+        <div className='mt-20 grid lg:grid-cols-2 sm:grid-cols-1 lg:px-32 px-4'>
             <div>
-                <img className='lg:h-2/4' src={opi2} alt="" />
+                <img className='lg:h-3/5' src={opi2} alt="" />
             </div>
             <div className='mt-3'>
-                <h1 className='lg:text-6xl sm:text-6xl lg:font-extrabold font-bold lg:mt-4 text-primary'>MEHEDI BANGLADESH</h1>
-                <h1>Creative Web Developer</h1>
+                <h1 className='lg:text-4xl sm:text-6xl lg:font-extrabold font-bold lg:mt-4 text-primary lg:mb-5'>MEHEDI</h1>
+                <h1 className='lg:text-4xl sm:text-6xl lg:font-extrabold font-bold lg:mt-4 text-primary lg:mb-5'>BANGLADESH</h1>
+                <h1 className='mt-3 lg:mb-5 mb-3'>Creative Web Developer</h1>
                 <p>
                     My name is Mehedi Hasan Opi. I am a Web Developer, and I'm very
                      passionate and dedicated to my work. 
@@ -17,6 +18,9 @@ const About = () => {
                      I enjoy every step of the design
                     process, from discussion and collaboration.
                 </p>
+                <button className="btn btn-primary hover:bg-white hover:text-primary mt-4">
+                            Learn More
+                        </button>
             </div>
         </div>
     );
