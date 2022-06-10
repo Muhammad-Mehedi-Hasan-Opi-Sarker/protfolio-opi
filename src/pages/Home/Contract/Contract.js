d85bd47fe422e209590fe8cf26ac4f84a4000830
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-06-10 21:17:53 +0600

    size input feild

--- src/pages/Home/Contract/Contract.js
+++ src/pages/Home/Contract/Contract.js
@@ -7,9 +7,9 @@ const Contract = () => {
     return (
         <div className='lg:px-32 grid justify-center mb-20 px-8' id='contrac'>
             <form onClick={handleContract}>
-            <input type="text" placeholder="Name" class="mb-5 input input-bordered input-primary w-full max-w-xs" /><br />
-            <input type="email" placeholder="Email" className="mb-5 input input-bordered input-primary w-full max-w-xs" /><br />
-            <textarea class="textarea textarea-primary" placeholder="Message"></textarea><br />
+            <input type="text" placeholder="Name" className="mb-5 input input-bordered input-primary lg:w-96" /><br />
+            <input type="email" placeholder="Email" className="mb-5 input input-bordered input-primary lg:w-96" /><br />
+            <textarea className="textarea textarea-primary lg:w-96 w-48 lg:h-60" placeholder="Message"></textarea><br />
             <input type="submit" className='btn btn-primary hover:bg-white hover:text-primary' value="Submit Message" />
             </form>
         </div>
